e3829f4d694b7bd6755309ba3af2ecf9802f6f70
n.nnah@yahoo.com <n.nnah@yahoo.com>
2024-07-15 22:56:46 +0100

    build exe

---
+++ models/post.go
@@ -0,0 +1,11 @@
+package models
+
+// Reward represents rewards earned by users
+type Post struct {
+	Model
+	UserID               uint      `json:"user_id" gorm:"foreignKey:ID"`
+	Title           string   `json:"post"`
+	PostCategory       string `json:"post_category"`
+	Image            string    `json:"string"`
+	PostDescription   string   `json:"post_description"`
+}
