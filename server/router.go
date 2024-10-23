37c9b1dc2c5c91ee607b50a1ceb046abe4a224d3
n.nnah@yahoo.com <techagentng@gmail.com>
2024-10-23 10:04:52 +0100

    trying to fix cors2

--- server/router.go
+++ server/router.go
@@ -60,7 +60,7 @@ func (s *Server) setupRouter() *gin.Engine {
 	// }
 	// Use CORS middleware with appropriate configuration
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"https://citizenx.ng", "http://localhost:3001"}, 
+		AllowOrigins:     []string{"https://citizenx.ng", "http://localhost:3001", "https://citizenx-9hk2.onrender.com/api/v1/google/login"}, 
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
 		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
 		AllowCredentials: true, // cookies or tokens
