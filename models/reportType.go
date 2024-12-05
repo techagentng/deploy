e9283f3b648828afbf377c7f66a35cb5858adb56
n.nnah@yahoo.com <techagentng@gmail.com>
2024-12-05 13:47:09 +0100

    fix oauth

--- models/reportType.go
+++ models/reportType.go
@@ -6,6 +6,7 @@ import (
 	"github.com/google/uuid"
 )
 
+
 type ReportType struct {
 	ID                   uuid.UUID        `gorm:"type:uuid;primaryKey" json:"id"`
 	UserID               uint             `json:"user_id"`
