9e52aeb165d5152251240c538a115d9df689456d
n.nnah@yahoo.com <techagentng@gmail.com>
2024-12-06 19:06:09 +0100

    fix oauth2

--- server/router.go
+++ server/router.go
@@ -82,7 +82,7 @@ func (s *Server) defineRoutes(router *gin.Engine) {
 	apirouter.GET("/fb/auth", s.handleFBLogin())
 	apirouter.GET("fb/callback", s.handleFBCallback())
 	apirouter.GET("/incident_reports", s.handleGetAllReport()) //
-	apirouter.POST("/google/login", s.HandleGoogleLogin())
+	apirouter.GET("/google/login", s.HandleGoogleLogin())
 	apirouter.GET("/auth/google/callback", s.HandleGoogleCallback()) //
 	apirouter.GET("/incident_reports/state/:state", s.handleGetAllReportsByState())
 	apirouter.GET("/incident_reports/lga/:lga", s.handleGetAllReportsByLGA())
