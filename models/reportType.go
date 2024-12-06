3e59c20825a1ca6797f3abbdcc2334516915277d
n.nnah@yahoo.com <techagentng@gmail.com>
2024-12-06 15:03:23 +0100

    first deploy 06

--- models/reportType.go
+++ models/reportType.go
@@ -6,7 +6,6 @@ import (
 	"github.com/google/uuid"
 )
 
-
 type ReportType struct {
 	ID                   uuid.UUID        `gorm:"type:uuid;primaryKey" json:"id"`
 	UserID               uint             `json:"user_id"`
