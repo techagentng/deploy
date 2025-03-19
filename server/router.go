6690599eb147f00cbe9ffa72cd8dae254947a430
n.nnah@yahoo.com <techagentng@gmail.com>
2025-03-19 21:49:22 +0100

    fix get

--- server/router.go
+++ server/router.go
@@ -68,7 +68,7 @@ func (s *Server) defineRoutes(router *gin.Engine) {
 	apirouter.GET("/fb/auth", s.handleFBLogin())
 	apirouter.GET("fb/callback", s.handleFBCallback())
 	apirouter.GET("/incident_reports", s.handleGetAllReport()) 
-	apirouter.GET("/google/user/login", s.handleGoogleUserLogin())
+	apirouter.POST("/google/user/login", s.handleGoogleUserLogin())
 	apirouter.GET("/google/login", s.HandleGoogleLogin())
 	apirouter.GET("/auth/google/callback", s.HandleGoogleCallback()) //
 	apirouter.GET("/incident_reports/state/:state", s.handleGetAllReportsByState())
